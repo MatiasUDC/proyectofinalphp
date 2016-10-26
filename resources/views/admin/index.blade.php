ba21a78fb37053c4a9f441f75850653345dce134
cmcuriqueo <cmcuriqueo@udc.edu.ar>
2016-10-25 23:50:59 -0200

    nada

--- resources/views/admin/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('admin.template.layout')
-@section('title',"Administracion")
-@section('content')
-    @include('admin.template.partials.nav')
-    <div>
-        Bienvenido administrador!!!! xD
-    </div>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -30,14 +30,11 @@ Route::get('persona/{id}/delete','\App\Http\Controllers\PersonaController@destro
 Route::get('persona/{id}/deleteMsg','\App\Http\Controllers\PersonaController@DeleteMsg');
 /********************* persona ***********************************************/
 
-<<<<<<< HEAD
+
 Route::group(['prefix' => 'admin'], function()
 {
-
-    Route::get('/', 'AdministradorController@index');
     Route::resource('producto','ProductoController');
 	Route::resource('categoria','CategoriaController');
 
 });
-=======
->>>>>>> 2918a4e8fa5ea742110dd492af4916ceeb640a32
+
