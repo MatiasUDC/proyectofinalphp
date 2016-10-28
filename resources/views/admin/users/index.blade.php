12eb24c88406f7febf4431dbdb69a7ada0ef1d3c
moises <mccallizanavilca@udc.edu.ar>
2016-10-28 18:11:03 -0300

    se modifica componentes

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -5,10 +5,10 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Users</div>
+                    <div class="panel-heading">Listado de Usuarios</div>
                     <div class="panel-body">
 
-                        <a href="{{ url('/admin/users/create') }}" class="btn btn-primary pull-right btn-sm">Add New User</a>
+                        <a href="{{ url('/admin/users/create') }}" class="btn btn-primary pull-right btn-sm">Agregar Nuevo Usuario</a>
                         <br/>
                         <br/>
 
@@ -16,7 +16,7 @@
                             <table class="table table-borderless">
                                 <thead>
                                     <tr>
-                                        <th>ID</th><th>Name</th><th>Email</th><th>Actions</th>
+                                        <th>ID</th><th>Nombre</th><th>Email</th><th>Acciones</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -26,14 +26,14 @@
                                         <td><a href="{{ url('/admin/users', $item->id) }}">{{ $item->name }}</a></td><td>{{ $item->email }}</td>
                                         <td>
                                             <a href="{{ url('/admin/users/' . $item->id . '/edit') }}">
-                                                <button type="submit" class="btn btn-primary btn-xs">Update</button>
+                                                <button type="submit" class="btn btn-primary btn-xs">Actualizar</button>
                                             </a> /
                                             {!! Form::open([
                                                 'method'=>'DELETE',
                                                 'url' => ['/admin/users', $item->id],
                                                 'style' => 'display:inline'
                                             ]) !!}
-                                                {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
+                                                {!! Form::submit('Eliminar', ['class' => 'btn btn-danger btn-xs']) !!}
                                             {!! Form::close() !!}
                                         </td>
                                     </tr>
