61a3f3b118a0002aae17f735a55c221efe3f4db6
moises <mccallizanavilca@udc.edu.ar>
2016-10-29 15:14:17 -0300

    modificaciones de la plantilla

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -103,6 +103,7 @@
                             <ul class="dropdown-menu">
 
                             <li><a href="{{ url('/admin') }}">Panel de Admin <span class="sr-only">(current)</span></a></li>
+                            <li><a href="{{ url('/home') }}">Panel de Usuario <span class="sr-only">(current)</span></a></li>
 
                             <li><a href="{{ url('/admin/users') }}"><span class="glyphicon glyphicon-user"></span> Usuarios</a></li>
                              <li><a href="{{ url('/admin/roles') }}"><span class="glyphicon glyphicon-ok" ></span> Roles</a></li>
