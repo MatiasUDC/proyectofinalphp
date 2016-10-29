6ab8032fae36d0aed79617ffe9df7e72b5a510ba
moises <mccallizanavilca@udc.edu.ar>
2016-10-29 15:11:46 -0300

    panel de usuario y admin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Panel de Uusuario</div>
 
                 <div class="panel-body">
-esta conectado usuario                </div>
+estas conectado como usuario                </div>
             </div>
         </div>
     </div>
