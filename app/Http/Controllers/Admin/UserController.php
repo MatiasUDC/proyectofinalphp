64123c937fa0cf839affc31aa09ac70f97c0e412
moises <mccallizanavilca@udc.edu.ar>
2016-11-14 09:16:05 -0300

    actualización de editar usuario

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -85,7 +85,12 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        //
+
+
+        $user = User::findOrFail($id);
+    
+     return view('admin.user.edit', compact('user'));
+
     }
 
     /**
@@ -97,7 +102,55 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        
+        $this->validate($request, [
+            'nombre'      => 'required|max:100',
+            'apellido' => 'required|max:100',
+            'email'     => 'required|email',
+            'user'      => 'required|min:4|max:20',
+            'password'  => ($request->get('password') != "") ? 'required|confirmed' : "",
+            'type'      => 'required|in:user,admin',
+        ]);
+        
+        $user->nombre = $request->get('nombre');
+        $user->apellido = $request->get('apellido');
+        $user->email = $request->get('email');
+        $user->user = $request->get('user');
+        $user->type = $request->get('type');
+        $user->direccion = $request->get('direccion');
+        $user->activo = $request->has('activo') ? 1 : 0;
+        if($request->get('password') != "") $user->password = $request->get('password');
+        
+       // $updated = $user->save();
+        
+       
+        
+        //return redirect()->route('admin.user.index')->with('message', $message);
+
+
+
+
+
+
+
+        $requestData = $request->all();
+        $user = User::findOrFail($id);
+
+        //$product->activo = $request->has('activo') ? 1 : 0;
+
+        $user->update($requestData);
+
+
+         $message = $updated ? 'Usuario actualizado correctamente!' : 'El Usuario NO pudo actualizarse!';
+
+
+        return redirect('/user')->with('message', $message);
+
+
+
+
+
+
     }
 
     /**
