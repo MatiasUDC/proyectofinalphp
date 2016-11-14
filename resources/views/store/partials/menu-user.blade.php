bf0532609873f5e2b982d7572a604f41bbcddf10
moises <mccallizanavilca@udc.edu.ar>
2016-11-14 16:10:59 -0300

    cambiando la ruta debería ser de route a url

--- resources/views/store/partials/menu-user.blade.php
+++ resources/views/store/partials/menu-user.blade.php
@@ -13,8 +13,8 @@
 			<i class="fa fa-user"></i> <span class="caret"></span>
 		</a>
 		<ul class="dropdown-menu" role="menu">
-			<li><a href="{{ route('login-get') }}">Iniciar sesión</a></li>
-			<li><a href="{{ route('register-get') }}">Registrarse</a></li>
+			<li><a href="{{ url('login-get') }}">Iniciar sesión</a></li>
+			<li><a href="{{ url('register-get') }}">Registrarse</a></li>
 		</ul>
 	</li>
 @endif
