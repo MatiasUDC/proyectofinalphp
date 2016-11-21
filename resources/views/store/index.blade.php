a0109606ab7f677eafd522483a58a91f47f65212
moises <mccallizanavilca@udc.edu.ar>
2016-11-21 16:59:49 -0300

    cambiando compra

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -9,15 +9,15 @@
 		@foreach($products as $product)
 			<div class="product white-panel">
 				<h3>{{ $product->name }}</h3><hr>
-				<img src="{{ $product->image }}" width="200">
+				<img src="{{ $product->image }}" width="200"> 
 				<div class="product-info panel">
 					<p>{{ $product->extract }}</p>
 					<h3><span class="label label-success">Precio: ${{ number_format($product->price,2) }}</span></h3>
 					<p>
 						<a class="btn btn-warning" href="{{ route('cart-add', $product->slug) }}">
-							<i class="fa fa-cart-plus"></i> La quiero
+							<i class="fa fa-cart-plus"></i> Comprar
 						</a>
-						<a class="btn btn-primary" href="{{ route('product-detail', $product->slug) }}"><i class="fa fa-chevron-circle-right"></i> Leer mas</a>
+						<a class="btn btn-primary" href="{{ route('product-detail', $product->slug) }}"><i class="fa fa-chevron-circle-right"></i> Ver mas</a>
 					</p>
 				</div>
 			</div>
